7bd2b9265a4db2fec0c68c71e10fa272533671ab
Cortalo <Cortalo@outlook.com>
2026-01-13 14:55:03 +0100

    feat: add pay-service with Feign client integration

---
+++ pay-service/src/main/java/com/hmall/pay/PayApplication.java
@@ -0,0 +1,17 @@
+package com.hmall.pay;
+
+import com.hmall.api.config.DefaultFeignConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+// only enable the Feign logger when you need it, otherwise it may harm performance
+@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = DefaultFeignConfig.class)
+@MapperScan("com.hmall.pay.mapper")
+@SpringBootApplication
+public class PayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+}
