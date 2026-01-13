5d6fc439d76ddbdada9c4b2138ec7b9e7b6b7c43
Cortalo <Cortalo@outlook.com>
2026-01-13 17:42:48 +0100

    feat: implement user info interceptor and context propagation
    - Added UserInfoInterceptor to extract user-info from headers into ThreadLocal
    - Configured MvcConfig with auto-configuration for Spring MVC services
    - Refactored CartServiceImpl to use UserContext instead of hardcoded ID

---
+++ hm-common/src/main/java/com/hmall/common/interceptors/UserInfoInterceptor.java
@@ -0,0 +1,29 @@
+package com.hmall.common.interceptors;
+
+import cn.hutool.core.util.StrUtil;
+import com.hmall.common.utils.UserContext;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserInfoInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1. get user info
+        String userInfo = request.getHeader("user-info");
+
+        // 2. determine if get user info
+        if (StrUtil.isNotBlank(userInfo)) {
+            UserContext.setUser(Long.valueOf(userInfo));
+        }
+
+        // 3. store in ThreadLocal
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserContext.removeUser();
+    }
+}
